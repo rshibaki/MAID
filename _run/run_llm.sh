ad5808889d27b7e4829516fd9a20a5c39694f688
rshibaki_l40s <ryota.shibaki@gmail.com>
2025-08-25 17:33:02 +0900

    scripts

---
+++ _run/run_llm.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#$ -S /bin/sh
+#$ -ac d=nvcr-pytorch-2309,d_shm=64G
+#$ -j y
+
+. /fefs/opt/dgx/env_set/nvcr-pytorch-2309-py3.sh
+
+mkdir -p ~/.raiden/nvcr-pytorch-2309
+export PATH="${HOME}/.raiden/nvcr-pytorch-2309/bin:$PATH"
+export LD_LIBRARY_PATH="${HOME}/.raiden/nvcr-pytorch-2309/lib:$LD_LIBRARY_PATH"
+export LDFLAGS=-L/usr/local/nvidia/lib64
+export PYTHONPATH="${HOME}/.raiden/nvcr-pytorch-2309/lib/python3.10/site-packages"
+export PYTHONUSERBASE="${HOME}/.raiden/nvcr-pytorch-2309"
+export PREFIX="${HOME}/.raiden/nvcr-pytorch-2309"
+
+export MY_PROXY_URL="http://10.1.10.1:8080/"
+export HTTP_PROXY=$MY_PROXY_URL
+export HTTPS_PROXY=$MY_PROXY_URL
+export FTP_PROXY=$MY_PROXY_URL
+export http_proxy=$MY_PROXY_URL
+export https_proxy=$MY_PROXY_URL
+export ftp_proxy=$MY_PROXY_URL
+
+pip install -r requirements.txt
+cd /hss/gMAI/RShibaki_tmp/MAID
+
+
+python3 run_llm.py --model qwen2.5 --prompt e4 --engine hf
+
